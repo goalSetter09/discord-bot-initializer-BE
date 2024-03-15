c14ce9fdebab34a27c5e03c8d2aa091ffc30fde5
dongyoung <dong0579@naver.com>
2024-03-15 13:15:54 +0900

    Revert "test commit"
    This reverts commit e5c5a960f788383d98214bc6fc4c2da7ece991d8.

--- src/main/java/hongik/discordbots/initializer/DiscordBotInitializerApplication.java
+++ src/main/java/hongik/discordbots/initializer/DiscordBotInitializerApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DiscordBotInitializerApplication {
 
 	public static void main(String[] args) {
-		System.out.println("test");
 		SpringApplication.run(DiscordBotInitializerApplication.class, args);
 	}
 
