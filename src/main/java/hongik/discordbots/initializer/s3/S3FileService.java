6443911626cf1313e2b6a54c15d6d17f18687268
Dongyoung Kim <145846246+goalSetter09@users.noreply.github.com>
2024-11-07 21:20:00 +0900

    feat: s3에서 특정 파일 내용 가져오는 매서드 구현

--- src/main/java/hongik/discordbots/initializer/s3/S3FileService.java
+++ src/main/java/hongik/discordbots/initializer/s3/S3FileService.java
@@ -1,16 +1,18 @@
 package hongik.discordbots.initializer.s3;
 
-import static java.util.stream.Collectors.*;
-
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.ListObjectsV2Request;
 import com.amazonaws.services.s3.model.ListObjectsV2Result;
+import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
 
 import lombok.RequiredArgsConstructor;
@@ -24,15 +26,27 @@ public class S3FileService {
 	@Value("${cloud.aws.s3.bucketName}")
 	private String bucketName;
 
+	private final static String botDirectoryPrefix = "discord-bot/";
+
 	// S3 버킷에서 봇 파일 목록을 가져오는 메서드
 	public List<String> listBotFiles() {
 		ListObjectsV2Request listObjectsRequest = new ListObjectsV2Request()
 			.withBucketName(bucketName)
-			.withPrefix("discord-bot/");  // 봇 파일이 저장된 경로
+			.withPrefix(botDirectoryPrefix);  // 봇 파일이 저장된 경로
 
 		ListObjectsV2Result listObjectsResult = amazonS3Client.listObjectsV2(listObjectsRequest);
 		return listObjectsResult.getObjectSummaries().stream()
 			.map(S3ObjectSummary::getKey)
+			.filter(key -> !key.endsWith("/")) // 폴더 이름이 아닌 실제 파일만 남김
 			.toList();
+	}
+
+	// S3에서 특정 파일의 내용을 가져오는 메서드
+	public String getFileContent(String s3Path) throws IOException {
+		String s3Key = botDirectoryPrefix + s3Path;
+
+		S3Object s3Object = amazonS3Client.getObject(new GetObjectRequest(bucketName, s3Key));
+		InputStream inputStream = s3Object.getObjectContent();
+		return new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
 	}
 }
