08da7a673de152d8d9e346a947cc4d4f0c3efaf4
Dongyoung Kim <145846246+goalSetter09@users.noreply.github.com>
2024-11-10 00:14:43 +0900

    chore: start-server.sh 기존 image를 삭제후 pull 하도록 변경

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -3,6 +3,7 @@
 echo "--------------- 서버 배포 시작 -----------------"
 docker stop initializer-server || true
 docker rm initializer-server || true
+docker rmi -f 479084400092.dkr.ecr.ap-northeast-2.amazonaws.com/initializer-server:latest || true
 docker login -u AWS -p $(aws ecr get-login-password --region ap-northeast-2) 479084400092.dkr.ecr.ap-northeast-2.amazonaws.com
 docker pull --no-cache 479084400092.dkr.ecr.ap-northeast-2.amazonaws.com/initializer-server:latest
 docker run -d --name initializer-server -p 8080:8080 479084400092.dkr.ecr.ap-northeast-2.amazonaws.com/initializer-server:latest
